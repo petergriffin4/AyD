43b72f74dc2194d38ddfcb899f119ce2900c30bd
petergriffin4 <feacuna@uamv.edu.ni>
2024-10-30 08:34:57 -0600

    Add files via upload

---
+++ Entradaejemplo/Entradaejemplo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entradaejemplo
+{
+    public partial class Producto : MetroFramework.Forms.MetroForm
+    {
+        public Producto()
+        {
+            InitializeComponent();
+            this.StyleManager = metroStyleManager1;
+        }
+
+        private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(metroToggle1.Checked)
+            {
+                metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            } else
+            {
+                metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            }
+        }
+    }
+}
